25e64530488de56e39200536d8262a9657e9e1b3
aarju09 <55630203+aarju09@users.noreply.github.com>
2019-09-27 20:06:52 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,12 @@
+class Address
+{
+	String society;
+	String area;
+	long pin_code;
+	String city;
+	Address(String society,String area,long pin_code,String city)
+	{
+      System.out.println("Address is:  "+society +area +pin_code+city);
+      
+	}
+}
---
+++ Qualification.java
@@ -0,0 +1,11 @@
+class Qualification
+{
+	String school_name;
+	int percentage;
+	String city;
+	Qualification(String school_name,int percentage,String city)
+	{
+     System.out.println("Educational Deatails are: "+school_name +percentage +city);
+     
+	}
+}
