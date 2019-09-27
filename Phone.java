1e095080370ddb461d406dec1d47d955edfb1c26
aarju09 <55630203+aarju09@users.noreply.github.com>
2019-09-27 20:07:18 +0530

    Add files via upload

---
+++ Phone.java
@@ -0,0 +1,12 @@
+class Phone
+{ 
+	String purpose;
+	String number;
+	Phone(String purpose,String number)
+	{
+      //String purpose[]={"office","home"};
+      //String phone[]={"134","8654"};
+      
+      System.out.println("phone number is:  "+purpose +number);
+	}
+}
